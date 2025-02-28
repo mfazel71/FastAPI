52713dfbe8ecf6660424e903e38ad2993451726e
mfazel71 <mfazel@live.com>
2025-02-28 12:24:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+import requests
+from fastapi import FastAPI
 
-import requests  
-from fastapi import FastAPI  
-
-app = FastAPI()  
-
-# کلید API و CX را جایگزین کن
-GOOGLE_API_KEY = "AIzaSyCNC_HYgzwb9O4VURp1a5Mez6fhwHsH5qI"  
-SEARCH_ENGINE_ID = "93935e66217854acd"  
+app = FastAPI()
 
 @app.get("/search/")
-async def search_google(query: str):  
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}"  
-    response = requests.get(url)  
+async def search_duckduckgo(query: str):
+    url = f"https://api.duckduckgo.com/?q={query}&format=json"
+    response = requests.get(url)
     return response.json()
